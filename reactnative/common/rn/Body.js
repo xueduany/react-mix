d7f1550ea32f8b206b06458305e9cef3b251f6a8
Xuedy <xuedy@ctrip.com>
2016-04-26 14:40:23 +0800

    增加可以滚动

--- reactnative/common/rn/Body.js
+++ reactnative/common/rn/Body.js
@@ -1,4 +1,8 @@
 class Body extends Div{
-	
+	render(){
+		return (
+			<ScrollView style={{height:this.props.height||(Dimensions.height-20)}}>{super.render()}</ScrollView>
+		)
+	}
 }
 module.exports = Body;
